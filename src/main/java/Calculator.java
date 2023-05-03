d63807f494a9ab967a0c55061f27b465f9e89dbb
smoser-LiL <smoser-LiL@localhost>
2023-05-03 17:43:16 +0000

    Moving files  using main

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,23 +1,22 @@
 public class Calculator {
 
-    public int add(int x, int y) {
-        return x + y;
-    }
+  public int add(int x, int y) {
+    return x + y;
+  }
 
-    // This method should return the result of subtracting y from x.
-    public int subtract(int x, int y) {
-        return 0;
-    }
+  // This method should return the result of subtracting y from x.
+  public int subtract(int x, int y) {
+    return x - y;
+  }
 
-    // This method should return the result of multiplying x and y.
-    public int multiply(int x, int y) {
-        return 0;
-    }
+  // This method should return the result of multiplying x and y.
+  public int multiply(int x, int y) {
+    return x * y;
+  }
 
-    // This method should return the result of diving x by y.
-    public int divide(int x, int y) {
-        return 0;
-    }
-
+  // This method should return the result of diving x by y.
+  public int divide(int x, int y) {
+    return x / y;
+  }
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,15 +4,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+  Calculator calculator = new Calculator();
 
-    @Test
-    public void testAdd() {
-        int expected = 3;
+  @Test
+  public void testAdd() {
+    int expected = 3;
 
-        int actual = calculator.add(1, 2);
+    int actual = calculator.add(1, 2);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testSubtract() {
+    int expected = 3;
+
+    int actual = calculator.subtract(6, 3);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testMultiply() {
+    int expected = 3;
+
+    int actual = calculator.multiply(1, 3);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testDivide() {
+    int expected = 3;
+
+    int actual = calculator.divide(15, 5);
+
+    assertEquals(expected, actual);
+  }
 
 }
